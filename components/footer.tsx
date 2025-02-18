cf392f03d079af77f6a7e3a2a5324dfd5a26bc65
Md. Redwan Ahmed <34208303+redwan-cse@users.noreply.github.com>
2025-02-18 15:10:06 +0600

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,12 +12,22 @@ export function Footer() {
         </div>
         <SocialLinks />
         <div className="mt-8 text-center">
-          <p className="text-sm text-muted-foreground">
+            <p className="text-sm text-muted-foreground">
             © {new Date().getFullYear()} Md Redwan Ahmed. All rights reserved.
-          </p>
-          <p className="mt-2 text-sm text-muted-foreground">
-            Founder & CEO  - Fast Cyber Defense
-          </p>
+            </p>
+            <p className="mt-2 text-sm text-muted-foreground">
+            <>
+            Founder & CEO - {' '}
+              <a href="https://fastcyberdefense.com/" className="hover:underline inline-flex items-center gap-2">
+              Fast Cyber Defense
+              <img 
+                src="https://fastcyberdefense.com/favicon.ico" 
+                alt="Fast Cyber Defense Logo" 
+                className="w-4 h-4"
+              />
+              </a>
+            </>
+            </p>
         </div>
       </div>
     </footer>
